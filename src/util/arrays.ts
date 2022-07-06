f4cc270803bd6bea969513da741467d68a0648cd
TriDvaRas <02dimon02@gmail.com>
2022-07-06 22:02:43 +0300

    reset

--- src/util/arrays.ts
+++
@@ -1,7 +0,0 @@
-export function shuffle(array: any[]) {
-    for (let i = 0; i < array.length; i++) {
-        const j: any = Math.floor(Math.random() * array.length) as Number
-        [array[i], array[j]] = [array[j], array[i]]
-    }
-    return array
-}
